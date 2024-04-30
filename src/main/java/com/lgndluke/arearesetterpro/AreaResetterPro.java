d53acd8e04d061dc34c5b9746b9d4f6a27331822
lgndluke <github@lgndluke.com>
2024-04-30 12:14:18 +0200

    Initial commit including paper-api update to version 1.20.4

--- src/main/java/com/lgndluke/arearesetterpro/AreaResetterPro.java
+++ src/main/java/com/lgndluke/arearesetterpro/AreaResetterPro.java
@@ -31,7 +31,7 @@ public final class AreaResetterPro extends JavaPlugin {
         DatabaseHandler.initialize();
 
         CommandLoader.load(this);
-        ListenerLoader.load(this);
+        ListenerLoader.load();
 
     }
 
